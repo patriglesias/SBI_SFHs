f1bf1b99ed1750d6d97faf4d919ba2de01280b14
patriglesias <sombra.1999@hotmail.com>
2022-11-22 14:00:17 +0000

    git push

---
+++ spender/instrument.py
@@ -0,0 +1,115 @@
+import torch, os
+import numpy as np
+from torch import nn
+from torchinterp1d import Interp1d
+
+class BaseInstrument(nn.Module):
+    """Base class for instruments
+
+    Container for wavelength vector, LSF and calibration functions.
+
+    CAUTION:
+    Don't base concrete implementations on this class, use :class:`Instrument` instead!
+
+    Parameters
+    ----------
+    wave_obs: `torch.tensor`
+        Observed wavelengths
+    lsf: :class:`LSF`
+        (optional) Line spread function model
+    calibration: callable
+        (optional) function to calibrate the observed spectrum
+    """
+    def __init__(self,
+                 wave_obs,
+                 lsf=None,
+                 calibration=None,
+                ):
+
+        super(BaseInstrument, self).__init__()
+
+        self.calibration = calibration
+        if lsf is not None:
+            assert isinstance(lsf, (LSF, torch.Tensor))
+            if isinstance(lsf, LSF):
+                self.lsf = lsf
+            else:
+                self.lsf = LSF(lsf)
+        else:
+            self.lsf = None
+
+        # register wavelength tensors on the same device as the entire model
+        self.register_buffer('wave_obs', wave_obs)
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+class LSF(nn.Conv1d):
+    def __init__(self, kernel, requires_grad=True):
+        super(LSF, self).__init__(1, 1, len(kernel), bias=False, padding='same')
+        # if LSF should be fit, set `requires_grad=True`
+        self.weight = nn.Parameter(kernel.flip(0).reshape(1,1,-1), requires_grad=requires_grad)
+
+    def forward(self, x):
+        # convolution with flux preservation
+        return super(LSF, self).forward(x) / self.weight.sum()
+
+def get_skyline_mask(wave_obs, min_intensity=2, mask_size=5):
+    """Return vector that masks the major skylines
+
+    For ever line in the skyline list in the file `data/sky-lines.txt` that is brighter
+    than a threshold, this method creates a mask whose size scales logarithmically with
+    line brightness.
+
+    Parameter
+    ---------
+    wave_obs: `torch.tensor`
+        Observed wavelengths
+    min_intensity: float
+        Intensity threshold
+    mask_size: float
+        Number of spectral elements to mask on either side of the line. This number
+        is the minmum size for lines with `min_intensity`.
+    Returns
+    -------
+    mask, `torch.tensor` of dtype `bool` with same shape as `wave_obs`
+    """
+    this_dir, this_filename = os.path.split(__file__)
+    filename = os.path.join(this_dir, "data", "sky-lines.txt")
+    skylines = np.genfromtxt(filename, names=['wavelength', 'intensity', 'name', 'status'], dtype=None, encoding=None)
+    # wavelength in nm, need A
+    skylines['wavelength'] *= 10
+
+    mask = torch.zeros(len(wave_obs), dtype=torch.bool)
+    for line in skylines[skylines['intensity'] > min_intensity]:
+        # increase masking area with intensity
+        mask_size_ = mask_size * (1 + np.log10(line['intensity'] / min_intensity))
+        mask |= (wave_obs - line['wavelength']).abs() <  mask_size_
+    return mask
+
+# allow registry of new instruments
+# see https://effectivepython.com/2015/02/02/register-class-existence-with-metaclasses
+instrument_register = {}
+
+def register_class(target_class):
+    instrument_register[target_class.__name__] = target_class
+
+class Meta(type):
+    """Meta class to enable registration of instruments"""
+    def __new__(meta, name, bases, class_dict):
+        cls = type.__new__(meta, name, bases, class_dict)
+        # remove those that are directly derived from the base class
+        if BaseInstrument not in bases:
+            register_class(cls)
+        return cls
+
+class Instrument(BaseInstrument, metaclass=Meta):
+    """Instrument class
+
+    Container for wavelength vector, LSF and calibration functions.
+
+    See `spender.instrument.instrument_register` for all known classes that derive from
+    :class:`Instrument`.
+    """
+    pass
---
+++ spender/util.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.data import IterableDataset
+from itertools import chain
+import pickle, humanize, psutil, GPUtil, io, random
+from torchinterp1d import Interp1d
+
+############ Functions for creating batched files ###############
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else: return super().find_class(module, name)
+
+def load_batch(batch_name, subset=None):
+    with open(batch_name, 'rb') as f:
+        if torch.cuda.is_available():
+            batch = pickle.load(f)
+        else:
+            batch = CPU_Unpickler(f).load()
+
+    if subset is not None:
+        return batch[subset]
+    return batch
+
+class BatchedFilesDataset(IterableDataset):
+    """Creates a dataset from a list of batched files
+
+    This class allows the use of batched files, whose size can be optimized for loading
+    performance, as input for a :class:`torch.utils.data.DataLoader`, whose batch size
+    can be chosen indepdently to optimize training.
+
+    See https://medium.com/speechmatics/how-to-build-a-streaming-dataloader-with-pytorch-a66dd891d9dd
+    for details.
+
+    The file list and the items in each loaded file can be shuffled if desired.
+
+    Parameters
+    ----------
+    file_list: list(str)
+        List of filenames to load batches from
+    load_fct: callable
+        Function to return batch when given filename
+    shuffle: bool
+        Whether to shuffle the order of the batch files
+    shuffle_instance: bool
+        Whether to shuffle spectra within each batch
+
+    """
+    def __init__(self, file_list, load_fct, shuffle=False, shuffle_instance=False):
+        assert len(file_list), "File list cannot be empty"
+        self.file_list = file_list
+        self.shuffle = shuffle
+        self.shuffle_instance = shuffle_instance
+        self.load_fct = load_fct
+
+    def process_data(self, idx):
+        if self.shuffle:
+            idx = random.randint(0, len(self.file_list) -1)
+        batch_name = self.file_list[idx]
+        data = self.load_fct(batch_name)
+        data = list(zip(*data))
+        if self.shuffle_instance:
+            random.shuffle(data)
+        for x in data:
+            yield x
+
+    def get_stream(self):
+        return chain.from_iterable(map(self.process_data, range(len(self.file_list))))
+
+    def __iter__(self):
+        return self.get_stream()
+
+    def __len__(self):
+        return len(self.file_list)
+
+
+def mem_report():
+    print("CPU RAM Free: " + humanize.naturalsize( psutil.virtual_memory().available ))
+
+    if torch.cuda.device_count() ==0: return
+
+    GPUs = GPUtil.getGPUs()
+    for i, gpu in enumerate(GPUs):
+        print('GPU {:d} ... Mem Free: {:.0f}MB / {:.0f}MB | Utilization {:3.0f}%'.format(i, gpu.memoryFree, gpu.memoryTotal, gpu.memoryUtil*100))
+    return
+
+
+def resample_to_restframe(wave_obs,wave_rest,y,w,z):
+    wave_z = (wave_rest.unsqueeze(1)*(1 + z)).T
+    wave_obs = wave_obs.repeat(y.shape[0],1)
+    # resample observed spectra to restframe
+    yrest = Interp1d()(wave_obs, y, wave_z)
+    wrest =  Interp1d()(wave_obs, w, wave_z)
+
+    # interpolation = extrapolation outside of observed region, need to mask
+    msk = (wave_z<=wave_obs.min())|(wave_z>=wave_obs.max())
+    # yrest[msk]=0 # not needed because all spectral elements are weighted
+    wrest[msk]=0
+    return yrest,wrest

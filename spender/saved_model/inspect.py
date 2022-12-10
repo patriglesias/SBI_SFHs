34c8e1883815106f9344ca306f6e2976776f459d
patriglesias <sombra.1999@hotmail.com>
2022-12-10 11:25:51 +0000

    git push

--- spender/saved_model/inspect.py
+++ spender/saved_model/inspect.py
@@ -10,7 +10,7 @@ model_loaded=checkpoint['model']
 np.savetxt('./generate_latent_2/losses.txt',np.array(losses))
 """
 
-
+"""
 
 ### Load and visualize losses ###
 losses=np.loadtxt('./generate_latent_2/losses.txt')
@@ -24,13 +24,41 @@ plt.legend()
 plt.savefig('./generate_latent_2/losses.png')
 plt.show()
 
+
+"""
+
 ### load and visualize percentiles and latents###
-percent_pred=np.loadtxt('./generate_latent_2/y_test_pred.txt')
-latents=np.loadtxt('./generate_latent_2/latents.txt')
-percent=np.loadtxt('./generate_latent_2/y_test.txt')
-for i in range(100):
-    #plt.plot(np.arange(10)*10,percent_pred[i,:],'.')
-    plt.plot(np.arange(10)*10,percent[i,:],'.')
+
+test_set=1000
+batch_size=128
+
+percent_pred=np.load('./generate_latent_2/y_test_pred.npy',allow_pickle=True)
+latents=np.load('./generate_latent_2/latents.npy',allow_pickle=True)
+percent=np.load('./generate_latent_2/y_test.npy',allow_pickle=True)
+
+
+percent_pred_arr=np.zeros((test_set,10))
+latents_arr=np.zeros((test_set,10))
+percent_arr=np.zeros((test_set,10))
+
+
+for j in range(len(percent_pred)):
+    for i,x in enumerate(percent_pred[j]):
+        percent_pred_arr[batch_size*j+i,:]=x
+        
+for j in range(len(latents)):
+    for i,x in enumerate(latents[j]):
+        latents_arr[batch_size*j+i,:]=x
+        
+for j in range(len(percent)):
+    for i,x in enumerate(percent[j]):
+        percent_arr[batch_size*j+i,:]=x
+        
+
+
+for i in range(test_set): 
+    plt.plot(np.arange(10)*10,percent_pred_arr[i,:],'.')
+    #plt.plot(np.arange(10)*10,percent_arr[i,:],'.')
 plt.title('Percentiles')
 plt.xlabel('Percentiles (%)')
 plt.xticks(ticks=np.arange(10)*10)
@@ -39,8 +67,8 @@ plt.show()
 
 
 
-for i in range(100):
-    plt.plot(np.arange(10),latents[i,:],'.')
+for i in range(test_set):
+    plt.plot(np.arange(10),latents_arr[i,:],'.')
 plt.title('Latents')
 plt.xlabel('Component')
 plt.ylabel('Value')

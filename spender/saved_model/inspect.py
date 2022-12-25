1b9a2909378ea6340535eaa6d1f2a1a949a5ec04
patriglesias <sombra.1999@hotmail.com>
2022-12-25 18:12:05 +0000

    git push

--- spender/saved_model/inspect.py
+++ spender/saved_model/inspect.py
@@ -12,32 +12,34 @@ np.savetxt('./generate_latent_2/losses.txt',np.array(losses))
 
 
 """
+
 ### Load and visualize losses ###
-losses=np.loadtxt('./generate_latent_2/losses.txt')
+losses=np.loadtxt('./generate_latent_2/latent_16/losses.txt')
 #plot losses
 epochs=range(len(losses[:,0]))
-plt.plot(epochs,losses[:,0],label='Training loss')
-plt.plot(epochs,losses[:,1],label='Validation loss')
+plt.plot(epochs[20:],losses[20:,0],label='Training loss')
+plt.plot(epochs[20:],losses[20:,1],label='Validation loss')
 plt.xlabel('Epochs')
-plt.title('Loss')
+plt.title('Loss n_latent 16 ')
 plt.legend()
-plt.savefig('./generate_latent_2/losses.png')
+#plt.savefig('./generate_latent_2/latent_16/losses.png')
 plt.show()
-"""
 
+"""
 ### load and visualize percentiles and latents###
 
-test_set=100
-batch_size=100
+test_set=1000
+batch_size=1000
+n_latent=14
 
-percent_pred=np.load('./generate_latent_2/y_test_pred.npy',allow_pickle=True)
-latents=np.load('./generate_latent_2/latents.npy',allow_pickle=True)
-percent=np.load('./generate_latent_2/y_test.npy',allow_pickle=True)
+percent_pred=np.load('./generate_latent_2/latent_'+str(n_latent)+'/y_test_pred.npy',allow_pickle=True)
+latents=np.load('./generate_latent_2/latent_'+str(n_latent)+'/latents.npy',allow_pickle=True)
+percent=np.load('./generate_latent_2/latent_'+str(n_latent)+'/y_test.npy',allow_pickle=True)
 
 
-percent_pred_arr=np.zeros((test_set,10))
-latents_arr=np.zeros((test_set,10))
-percent_arr=np.zeros((test_set,10))
+percent_pred_arr=np.zeros((test_set,9))
+latents_arr=np.zeros((test_set,n_latent))
+percent_arr=np.zeros((test_set,9))
 
 
 for j in range(len(percent_pred)):
@@ -56,18 +58,19 @@ for j in range(len(percent)):
 
 
 
-for j in np.arange(1,10):
+for j in np.arange(1,10): #percentiles go from 10% to 90%
     for i in range(test_set): 
-        plt.plot(percent_arr[i,j],percent_pred_arr[i,j],'.')#,'k.')
-    x=np.arange(np.min(percent_arr[:,j]),np.max(percent_arr[:,j]))
+        plt.plot(percent_arr[i,j-1],percent_pred_arr[i,j-1],'.')#,'k.')
+    x=np.arange(np.min(percent_arr[:,j-1]),np.max(percent_arr[:,j-1]))
     plt.plot(x,x)
     #plt.title('Percentiles ')
-    plt.xlabel('Time percentile '+str(j)+' real (Gyrs)')
-    plt.ylabel('Time percentile '+str(j)+' predicted (Gyrs)')
+    plt.xlabel('Time percentile '+str(j*10)+' real (Gyrs)')
+    plt.ylabel('Time percentile '+str(j*10)+' predicted (Gyrs)')
     plt.show()
 
 """
-for j in range(10):
+
+for j in range(n_latent):
     plt.hist(latents_arr[:,j])
     plt.title('Latent '+str(j))
     plt.xlabel('Value')

f1251c9807df286463d89692da6d7f315d77afd8
patriglesias <sombra.1999@hotmail.com>
2023-01-28 13:50:46 +0000

    git push

--- spender/spender/spender_model.py
+++ spender/spender/spender_model.py
@@ -304,13 +304,13 @@ class Base_encoder_percentiles(nn.Module):
         #    loss_ind = torch.sum(0.5 *w* (y - y_).pow(2), dim=1) / y.shape[1]
 
         #L1 loss
-        #loss_l1=nn.L1Loss(size_average=None, reduce=None, reduction='mean')
-        #loss_ind = loss_l1(y, y_)
+        loss_ind = torch.sum(0.5 * torch.abs(y - y_), dim=1) / y.shape[1]
 
         #L2 loss
-        loss_l2=nn.MSELoss(size_average=None, reduce=None, reduction='mean')
-        loss_ind = loss_l2(y, y_)
+        #loss_ind = torch.sum(0.5 * (y - y_).pow(2), dim=1) / y.shape[1]
 
+        #log cosh loss
+        #loss_ind = torch.mean(torch.log(torch.cosh(y - y_ + 1e-12)))
 
 
         

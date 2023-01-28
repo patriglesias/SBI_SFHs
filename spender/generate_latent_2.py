abfd0d34be1ee7d1f6a1e618907122438145acbb
Patricia Iglesias Navarro <pin-ext@deimos.ll.iac.es>
2023-01-28 13:29:32 +0000

    with dropout

--- spender/generate_latent_2.py
+++ spender/generate_latent_2.py
@@ -217,7 +217,8 @@ if training_mode:
     # define and train the model
     print('Model defined')
     #(16,32,64)
-    model = encoder_percentiles(n_latent=n_latent,n_out=9,n_hidden=(16,16,16),act=None)
+    #(16,16,16)
+    model = encoder_percentiles(n_latent=n_latent,n_out=9,n_hidden=(16,32),act=None,dropout_2=None)
 
     train(model, trainloader, validloader, n_latent,n_epoch=max_epochs, n_batch=batch_size, outfile=None, losses=None, lr=lr, verbose=True)
 

d4a560d02caf934942021ee6674152e18c77fe33
patriglesias <sombra.1999@hotmail.com>
2023-01-28 14:09:19 +0000

    git push

--- spender/saved_model/inspect.py
+++ spender/saved_model/inspect.py
@@ -69,7 +69,7 @@ for j in np.arange(1,10): #percentiles go from 10% to 90%
     for i in range(test_set): 
         plt.plot(percent_arr[i,j-1],percent_pred_arr[i,j-1],'.')#,'k.')
     x=np.arange(np.min(percent_arr[:,j-1]),np.max(percent_arr[:,j-1]))
-    plt.plot(x,x)
+    #plt.plot(x,x)
     #plt.title('Percentiles ')
     plt.xlabel('Time percentile '+str(j*10)+' real (Gyrs)')
     plt.ylabel('Time percentile '+str(j*10)+' predicted (Gyrs)')

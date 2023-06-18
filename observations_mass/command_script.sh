1a27374450c469c5f5070519ca5c4ff9565f9e45
patriglesias <sombra.1999@hotmail.com>
2023-06-18 22:53:37 +0100

    git push

---
+++ observations_mass/command_script.sh
@@ -0,0 +1,1 @@
+python3.8 ./generate_input_obs.py && python3.8 ./train_encoder.py && python3.8 ./test_encoder.py  &&  python3.8 ./get_latents.py && python3.8 ./posterior_estimation.py
---
+++ observations_mass/spender/__init__.py
@@ -0,0 +1,33 @@
+import torch
+from .spender_model import MLP, SpectrumEncoder, encoder_percentiles
+
+
+#function tunned by Patricia
+def load_model(filename, device=None,n_latent=16,n_out=9,n_hidden=(16,16,16)):
+    """Load models from file
+
+    Parameter
+    ---------
+    filename: str
+        Path to file which contains the torch state dictionary
+
+    device: `torch.Device`
+        Device to load model structure into
+
+    Returns
+    -------
+    model: `torch.nn.Module`
+        The default :class`encoder_percentiles` model loaded from file
+    loss: `torch.tensor`
+        Traning and validation loss for this model
+    """
+
+    model_struct = torch.load(filename, map_location=device)
+
+    model = encoder_percentiles(n_latent=n_latent,n_out=n_out,n_hidden=n_hidden)
+
+    model.load_state_dict(model_struct['model'], strict=False)
+
+    loss = torch.tensor(model_struct['losses'])
+    return model, loss
+
